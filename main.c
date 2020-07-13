455d383d0c46a5b2e25e0ffa57ca3bc0079c1d21
Renat <renat@MacBook-Pro-2.local>
2020-07-13 15:44:49 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include "fdf.h"
+
+int dial_key(int key, fdf *data)
+{
+    ft_printf("%d\n", key);
+
+    if (key == 126)
+        data->shift_y -= 10;
+    if (key == 125)
+        data->shift_y += 10;
+    if (key == 123)
+        data->shift_x -= 10;
+    if (key == 124)
+        data->shift_x += 10;
+    mlx_clear_window(data->mlx_ptr, data->win_ptr); 
+    draw(data);
+    return (0);
+}
+
+int main(int argc, char **argv)
+{
+    fdf *data;
+    
+    data = (fdf*)malloc(sizeof(fdf));
+    read_file(argv[1], data);
+    data->mlx_ptr = mlx_init();
+    data->win_ptr = mlx_new_window(data->mlx_ptr, 600, 600, "FDF");
+    data->zoom =  20;
+
+    // bresenham(10, 10, 600, 300, data);
+    draw(data);
+    mlx_key_hook(data->win_ptr, dial_key, data);
+    mlx_loop(data->mlx_ptr);
+}
